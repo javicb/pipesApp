56fc105b6e90ca5bf37c883d4d46a075e6f99c61
Javi <javi.cb@gmail.com>
2021-01-28 12:07:11 +0100

    pipes uppercas, lowercase, titlecase

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+
+  nombreLower = 'lower';
+  nombreUpper = 'UPPER';
+  nombreCompleto = 'tItlE cAsE';
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
