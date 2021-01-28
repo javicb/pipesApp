a89bd877845b8958dd81b377df5f6d53f0441f7e
Javi <javi.cb@gmail.com>
2021-01-28 10:12:40 +0100

    primeros pipes y primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  nombre = 'javier cruz';
+  valor = 1000;
+  obj = {
+    nombre: 'javier'
+  };
+
+  mostrarNombre(): void {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
