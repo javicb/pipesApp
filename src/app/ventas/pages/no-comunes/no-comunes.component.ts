3f182c243d10566f68e91b560f9cc97527b89088
Javi <javi.cb@gmail.com>
2021-01-28 15:02:24 +0100

    async pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -46,6 +47,14 @@ export class NoComunesComponent implements OnInit {
       vuela: false
     }
   ];
+
+  // Async
+  miObservable = interval(2000); // 0,1,2,3,4,5,6,
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 
   constructor() { }
 
